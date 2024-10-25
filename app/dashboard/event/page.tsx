76eafade050c4fba49a817fb824c1b292ec0d212
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-10-24 23:42:56 -0500

    Fix Deployment Issue

--- app/dashboard/event/page.tsx
+++ app/dashboard/event/page.tsx
@@ -1,8 +1,8 @@
-import { Event } from "@/app/api/entities/event.entity";
-import React from "react";
+// import { Event } from "@/app/api/entities/event.entity";
+// import React from "react";
 
-type Props = { event: Event };
+// type Props = { event: Event };
 
-export default function page({ event }: Props) {
-  return <div>page</div>;
-}
+// export default function page({ event }: Props) {
+//   return <div>page</div>;
+// }
